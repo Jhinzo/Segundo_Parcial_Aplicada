006dc390ac8bdb6e01b0e4ca9ded24ba6a1992a1
Bleimy De Jesus Caceres <bleimy_dejesus@ucne.edu.do>
2020-07-06 18:20:36 -0400

    agregue utilidades y contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Segundo_Parcial_Aplicada.DAL
+{
+    public class Contexto : DbContext
+    {
+
+
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
