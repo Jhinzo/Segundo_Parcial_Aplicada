1e14e9d244e556ed52ec09e473ffca01eb32f286
Bleimy De Jesus Caceres <bleimy_dejesus@ucne.edu.do>
2020-07-06 21:09:07 -0400

    Update

--- Entidades/ProyectoDetalle.cs
+++ Entidades/ProyectoDetalle.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
 
 namespace Segundo_Parcial_Aplicada.Entidades
 {
@@ -11,12 +8,12 @@ namespace Segundo_Parcial_Aplicada.Entidades
         [Key]
         public int Id { get; set; }
         public int ProyectoId { get; set; }
-       
+
         public int TareaId { get; set; }
 
         [ForeignKey("TareaId")]
         public Tareas tipo { get; set; } = new Tareas();
-       
+
         public string Requerimiento { get; set; }
         public double Tiempo { get; set; }
     }
